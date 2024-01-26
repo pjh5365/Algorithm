788dbc3bc298d5af5f5d6492b99914b2286cbd94
박지혁 <pjh5365@naver.com>
2024-01-26 12:27:53 +0900

    [Gold II] Title: 확장 게임, Time: 752 ms, Memory: 84008 KB -BaekjoonHub

--- 백준/Gold/16920. 확장 게임/확장 게임.java
+++ 백준/Gold/16920. 확장 게임/확장 게임.java
@@ -13,14 +13,16 @@ public class Main {
 		int P = Integer.parseInt(st.nextToken());
 
 		int[][] arr = new int[N][M];
-		int[] move = new int[P + 1];
-		Queue<Pair>[] q = new Queue[P + 1];
+		ArrayList<Integer> move = new ArrayList<>();
+		ArrayList<Queue<Pair>> q = new ArrayList<>();
 		int[] result = new int[P + 1];
+		move.add(0);
+		q.add(new LinkedList<>());
 
 		st = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= P; i++) {
-			move[i] = Integer.parseInt(st.nextToken());
-			q[i] = new LinkedList<>();
+		for (int i = 0; i < P; i++) {
+			move.add(Integer.parseInt(st.nextToken()));
+			q.add(new LinkedList<>());
 		}
 
 		int zeroCount = 0;
@@ -31,9 +33,10 @@ public class Main {
 					arr[i][j] = -1;
 				else if (input.charAt(j) <= '9' && input.charAt(j) >= '1') {
 					arr[i][j] = input.charAt(j) - '0';
-					q[arr[i][j]].add(new Pair(i, j, 0, arr[i][j], 0));
+					q.get(arr[i][j]).add(new Pair(i, j, 0, arr[i][j], 0));
 					result[arr[i][j]]++;
-				} else
+				}
+				else
 					zeroCount++;
 			}
 		}
@@ -41,7 +44,9 @@ public class Main {
 		int[] checkX = {0, 0, 1, -1};
 		int[] checkY = {1, -1, 0, 0};
 
-		bfs(zeroCount, q, move, checkX, checkY, N, M, P, arr, result);
+		q.remove(0); // 0번은 사용하지 않으므로 삭제
+		move.remove(0);
+		bfs(zeroCount, q, move, checkX, checkY, N, M, arr, result);
 
 		for (int i = 1; i <= P; i++) {
 			sb.append(result[i]).append(" ");
@@ -51,16 +56,19 @@ public class Main {
 		bw.flush();
 	}
 
-	private static void bfs(int zeroCount, Queue<Pair>[] q, int[] move, int[] checkX,
-			int[] checkY, int N, int M, int P, int[][] arr, int[] result) {
+	private static void bfs(int zeroCount, ArrayList<Queue<Pair>> q, ArrayList<Integer> move, int[] checkX,
+			int[] checkY, int N, int M, int[][] arr, int[] result) {
 		int count = 0;
-		int beforeZeroCount;
 		while (zeroCount != 0) {
-			beforeZeroCount = zeroCount;
-			for (int i = 1; i <= P; i++) {
-				Queue<Pair> getQ = q[i];
-				if (getQ.peek() == null)	// 더 이상 확장할 수 없다면
+			int beforeZeroCount = zeroCount;
+			int i = 0;
+			while (i < q.size()) {
+				Queue<Pair> getQ = q.get(i);
+				if (getQ.peek() == null) {	// 더 이상 확장할 수 없다면
+					q.remove(i);
+					move.remove(i);
 					continue;
+				}
 				while (getQ.peek() != null && getQ.peek().count == count) {	// 꺼낸 요소의 순서가 맞는 경우에만
 					Pair poll = getQ.poll();
 
@@ -73,9 +81,9 @@ public class Main {
 
 						if (arr[x][y] == 0) {
 							arr[x][y] = arr[poll.x][poll.y];
-							if (poll.move + 1 < move[i]) // 이동을 다 하지 못한 경우에는 이동횟수를 늘려서 삽입
+							if (poll.move + 1 < move.get(i)) // 이동을 다 하지 못한 경우에는 이동횟수를 늘려서 삽입
 								getQ.add(new Pair(x, y, poll.count, arr[x][y], poll.move + 1));
-							else if (poll.move + 1 == move[i])
+							else if (poll.move + 1 == move.get(i))
 								getQ.add(new Pair(x, y, poll.count + 1, arr[x][y], 0));
 
 							result[poll.now]++;
@@ -85,6 +93,7 @@ public class Main {
 						}
 					}
 				}
+				i++;
 			}
 			if (zeroCount == beforeZeroCount)
 				return;
