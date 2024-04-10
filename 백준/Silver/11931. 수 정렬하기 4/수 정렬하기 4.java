5df19c62201066a75293c47dc7c7c8ee5e13b224
박지혁 <pjh5365@naver.com>
2024-04-10 12:10:13 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 1428 ms, Memory: 109712 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(arr);
+
+		for (int i = N - 1; i >= 0; i--) {
+			sb.append(arr[i]).append("\n");
+		}
+
+		bw.write(String.valueOf(sb));
+		bw.flush();
+	}
+}
