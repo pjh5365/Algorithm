04f74032dfc871b19e30b458dd725b5013c25a09
박지혁 <pjh5365@naver.com>
2024-04-24 11:37:58 +0900

    [Silver V] Title: 중복 빼고 정렬하기, Time: 432 ms, Memory: 29788 KB -BaekjoonHub

---
+++ 백준/Silver/10867. 중복 빼고 정렬하기/중복 빼고 정렬하기.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		int[] sorted = Arrays.stream(arr).sorted().distinct().toArray();
+		for (int i : sorted) {
+			sb.append(i).append(" ");
+		}
+		bw.write(String.valueOf(sb));
+		bw.flush();
+	}
+}
