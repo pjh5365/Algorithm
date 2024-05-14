1e36ec5cf3a2ce6c199a393e056204b7471db271
박지혁 <pjh5365@naver.com>
2024-05-15 08:11:48 +0900

    [Silver II] Title: 주식, Time: 1132 ms, Memory: 314632 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.java
+++ 백준/Silver/11501. 주식/주식.java
@@ -16,41 +16,22 @@ public class Main {
 			long max = 0;
 
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int maxIndex = 0;
 			for (int i = 0; i < N; i++) {
 				arr[i] = Long.parseLong(st.nextToken());
-				if (arr[i] >= max) {
-					max = Math.max(max, arr[i]);
-					maxIndex = i;
-				}
 			}
+			max = arr[N - 1];
 			int count = 0;
-			for (int i = 0; i < maxIndex; i++) {
-				if (arr[i] < max) { // 최대값보다 작다면 전부 구매
+			for (int i = N - 2; i >= 0; i--) { // 뒤에서부터 탐색
+				if (arr[i] < max) { // 주식을 구매하는 경우
 					result -= arr[i];
 					count++;
+				} else if (arr[i] > max) { // 최대값에 팔기
+					result += max * count;
+					count = 0;
+					max = arr[i];
 				}
 			}
-			result += count * max;
-			count = 0;
-			while (maxIndex < N - 1) { // 최대값이 마지막값이 될 때 까지
-				max = 0;
-				int startIndex = maxIndex + 1;
-				for (int i = maxIndex + 1; i < N; i++) {
-					if (arr[i] >= max) {
-						max = Math.max(max, arr[i]);
-						maxIndex = i;
-					}
-				}
-				for (int i = startIndex; i < maxIndex; i++) {
-					if (arr[i] < max) { // 최대값보다 작다면 전부 구매
-						result -= arr[i];
-						count++;
-					}
-				}
-				result += count * max;
-				count = 0;
-			}
+			result += max * count; // 탐색을 완료하고 마지막 값 더하기
 			sb.append(result).append("\n");
 			T--;
 		}
