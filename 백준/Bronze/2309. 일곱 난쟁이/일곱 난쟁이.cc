e78dda40ae7d6d2e787af2237a0e79c8da873e55
박지혁 <pjh5365@naver.com>
2024-07-31 16:29:34 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[9];
+    for (int i = 0; i < 9; i++)
+        cin >> arr[i];
+    sort(arr, arr + 9);
+
+    // 순열로 찾기
+    do
+    {
+        int sum = 0;
+        for (int i = 0; i < 7; i++)
+            sum += arr[i];
+        if (sum == 100)
+            break;
+    } while (next_permutation(arr, arr + 9));
+    for (int i = 0; i < 7; i++)
+        cout << arr[i] << '\n';
+}
