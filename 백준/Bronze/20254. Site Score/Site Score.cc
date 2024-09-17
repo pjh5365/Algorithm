f312958f1a90d0dd4d85000f77c8ec993e2131a5
박지혁 <pjh5365@naver.com>
2024-09-17 13:22:28 +0900

    [Bronze V] Title: Site Score, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a, b, c, d;
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  cin >> a >> b >> c >> d;
+  cout << 56 * a + 24 * b + 14 * c + 6 * d << '\n';
+}
