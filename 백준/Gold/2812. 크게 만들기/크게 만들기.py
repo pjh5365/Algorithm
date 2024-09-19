e651691103e6bb39e95a6f80ac329febdb9e16d2
박지혁 <pjh5365@naver.com>
2024-09-19 11:07:53 +0900

    [Gold III] Title: 크게 만들기, Time: 268 ms, Memory: 33960 KB -BaekjoonHub

--- 백준/Gold/2812. 크게 만들기/크게 만들기.py
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.py
@@ -2,12 +2,9 @@ N, K = map(int, input().split())
 num = input()
 s = []
 for i in num:
-	while (len(s) != 0 and K > 0):
-		if (s[-1] < i):
+	while (len(s) != 0 and K > 0 and s[-1] < i):
 			s.pop()
 			K -= 1
-		else:
-			break;
 	s.append(i)
 
 while K > 0:
