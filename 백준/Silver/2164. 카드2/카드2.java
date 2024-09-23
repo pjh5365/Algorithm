ea9158f9981176365c1b2c8ad7f210134c3cd29e
박지혁 <pjh5365@naver.com>
2024-09-23 14:55:48 +0900

    [Silver IV] Title: 카드2, Time: 160 ms, Memory: 45664 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -5,22 +5,22 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 
 		int N = Integer.parseInt(br.readLine());
 
-		Queue<Integer> queue = new LinkedList<>();
-
+		Queue<Integer> q = new LinkedList<>();
 		for (int i = 1; i <= N; i++) {
-			queue.add(i);
+			q.add(i);
 		}
 
-		while (queue.size() != 1) {
-			queue.poll();
-			int get = queue.poll();
-			queue.add(get);
+		while (q.size() != 1) {
+			q.poll();
+			q.add(q.poll());
 		}
 
-		bw.write(String.valueOf(queue.poll()));
+		bw.write(String.valueOf(q.poll()));
+		bw.newLine();
 		bw.flush();
 	}
 }
