9f7deaff8747ef5fecca8ab39f9990ccadbbcc46
박지혁 <pjh5365@naver.com>
2024-10-19 16:44:42 +0900

    [Bronze III] Title: 사과, Time: 104 ms, Memory: 14260 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10833. 사과/사과.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+		int ret = 0;
+
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+			ret += b % a;
+		}
+
+		bw.write(String.valueOf(ret));
+		bw.newLine();
+		bw.flush();
+	}
+}
