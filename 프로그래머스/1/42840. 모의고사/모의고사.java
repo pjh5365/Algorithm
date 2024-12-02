53bdc7da937556091089a881214520038f29462f
박지혁 <pjh5365@naver.com>
2024-12-02 10:57:54 +0900

    [level 1] Title: 모의고사, Time: 3.15 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,47 +2,32 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] one = {1, 2, 3, 4, 5};
-        int[] two = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[][] pattern = {
+            {1, 2, 3, 4, 5},
+            {2, 1, 2, 3, 2, 4, 2, 5},
+            {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+        };
         
-        int result1 = 0;
-        int result2 = 0;
-        int result3 = 0;
+        int[] sum = new int[3];
+        int max = 0;
         
-        int index1 = 0;
-        int index2 = 0;
-        int index3 = 0;
-        
-        for (int i : answers) {
-            if (one[index1] == i) {
-                result1++;
+        for (int i = 0; i < answers.length; i++) {
+            for (int j = 0; j < pattern.length; j++) {
+                if (answers[i] == pattern[j][i % pattern[j].length]) {
+                    sum[j]++;
+                    max = Math.max(max, sum[j]);
+                }
             }
-            if (two[index2] == i) {
-                result2++;
-            }
-            if (three[index3] == i) {
-                result3++;
-            }
-            index1++;
-            index1 %= 5;
-            index2++;
-            index2 %= 8;
-            index3++;
-            index3 %= 10;
         }
-        int max = Math.max(result1, Math.max(result2, result3));
+        
         ArrayList<Integer> list = new ArrayList<>();
-        if (max == result1)
-            list.add(1);
-        if (max == result2)
-            list.add(2);
-        if (max == result3)
-            list.add(3);
-        int[] result = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            result[i] = list.get(i);
+        
+        for (int i = 0; i < 3; i++) {
+            if (sum[i] == max) {
+                list.add(i + 1);
+            }
         }
-        return result;
+        
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
