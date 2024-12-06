949e86e4de02829b8bfac12729d97b2cf85c2f09
박지혁 <pjh5365@naver.com>
2024-12-06 10:30:29 +0900

    [Bronze III] Title: ZOAC 5, Time: 224 ms, Memory: 20192 KB -BaekjoonHub

---
+++ 백준/Bronze/26594. ZOAC 5/ZOAC 5.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String s = br.readLine();
+
+		char tmp = s.charAt(0);
+		int ret = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (tmp == s.charAt(i)) {
+				ret++;
+			} else {
+				break;
+			}
+		}
+
+		bw.write(String.valueOf(ret));
+		bw.flush();
+	}
+}
