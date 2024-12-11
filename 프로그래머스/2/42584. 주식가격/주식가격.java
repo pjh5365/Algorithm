eb224c567e8e0bc56bd03f8cbb3fd6afc7327376
박지혁 <pjh5365@naver.com>
2024-12-11 09:44:00 +0900

    [level 2] Title: 주식가격, Time: 40.02 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,39 +2,27 @@ import java.util.*;
 
 class Solution {
     /**
-    * prices의 idx를 스택에 넣기?
+    * prices의 idx를 스택에 넣어 하나의 스택만 사용하기
     */
     public int[] solution(int[] prices) {
-        Stack<Integer> s = new Stack<>(); // 가격을 담을 스택
-        Stack<Integer> time = new Stack<>(); // 입력된 시간을 담을 스택
+        Stack<Integer> s = new Stack<>(); // 가격을 idx를 담을 스택
         int len = prices.length;
         int[] answer = new int[len];
         
-        for (int i = 0; i < len; i++) {
-            int now = prices[i];
-            if (s.isEmpty()) {
-                s.push(now);
-                time.push(i);
-            } else {
-                if (s.peek() > now) {
-                    while (!s.isEmpty() && s.peek() > now) {
-                        s.pop();
-                        int tmp = time.pop();
-                        answer[tmp] = i - tmp;   
-                    }
-                    s.push(now);
-                    time.push(i);
-                } else {
-                    s.push(now);
-                    time.push(i);
-                }
-            }
-        }
-        while (!time.isEmpty()) {
-            int tmp = time.pop();
-            answer[tmp] = len - tmp - 1;
-        }
+        s.push(0); // 0번 idx는 무조건 입력
         
+        for (int i = 1; i < len; i++) {
+            // 스택이 비어있지 않고, 새로운 가격이 현재 가격보다 낮다면 (가격이 떨어졌다면)
+            while (!s.isEmpty() && prices[i] < prices[s.peek()]) {
+                int tmp = s.pop();
+                answer[tmp] = i - tmp; // 기존의 가격에 떨어지지 않은 길이 입력하기
+            }
+            s.push(i); // 가격이 떨어지지 않았다면
+        }
+        while (!s.isEmpty()) { // 스택에 남은 나머지 연산하기
+            int tmp = s.pop();
+            answer[tmp] = len - 1 - tmp;
+        }
         return answer;
     }
 }
