390302d61537804dab4a7e0d0d58b6cf3401f84b
박지혁 <pjh5365@naver.com>
2024-12-16 12:33:23 +0900

    [Bronze V] Title: SciComLove, Time: 100 ms, Memory: 14088 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			sb.append("SciComLove \n");
+		}
+
+		bw.write(String.valueOf(sb));
+		bw.flush();
+	}
+}
