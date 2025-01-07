8ee3572d8f8b5683a88386fbbc282fb6f6bec7a2
박지혁 <pjh5365@naver.com>
2025-01-07 19:23:11 +0900

    [Bronze V] Title: CAPS, Time: 260 ms, Memory: 21168 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String s = br.readLine();
+
+		bw.write(s.toUpperCase());
+		bw.flush();
+	}
+}
