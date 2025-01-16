416e05e6a0c86954237f7768b51ca76de007baf0
박지혁 <pjh5365@naver.com>
2025-01-16 19:28:47 +0900

    [Bronze V] Title: 이상한 기호, Time: 104 ms, Memory: 14272 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		long A = Long.parseLong(st.nextToken());
+		long B = Long.parseLong(st.nextToken());
+
+		long ret = (A + B) * (A - B);
+
+		bw.write(String.valueOf(ret));
+		bw.flush();
+	}
+}
