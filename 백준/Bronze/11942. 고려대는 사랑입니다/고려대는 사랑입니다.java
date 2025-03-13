fde37fd71087ed121a7e424d86cf6d5cdac9f9a4
박지혁 <pjh5365@naver.com>
2025-03-13 20:44:09 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 96 ms, Memory: 13968 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        bw.write("고려대학교");
+        bw.flush();
+    }
+}
