fd9345bad937735ee4e06ef4d663c197c18a8925
박지혁 <pjh5365@naver.com>
2025-03-25 18:15:15 +0900

    [Bronze II] Title: 야바위 대장, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/15814. 야바위 대장/야바위 대장.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String S = br.readLine();
+        char[] arr = S.toCharArray();
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            char tmp = arr[A];
+            arr[A] = arr[B];
+            arr[B] = tmp;
+        }
+
+        bw.write(new String(arr));
+        bw.flush();
+    }
+}
