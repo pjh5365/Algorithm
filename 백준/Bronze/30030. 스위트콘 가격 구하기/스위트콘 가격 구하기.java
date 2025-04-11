b2092fa208af37315926747038f7a37a5ac12826
박지혁 <pjh5365@naver.com>
2025-04-11 18:44:33 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = Integer.parseInt(br.readLine());
+        int ret = a / 110 * 100;
+
+        bw.write(String.valueOf(ret));
+        bw.flush();
+    }
+}
