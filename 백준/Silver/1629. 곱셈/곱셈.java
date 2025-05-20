613953d8531ca4915fceb4a63679ce4034d206e7
박지혁 <pjh5365@naver.com>
2025-05-20 20:45:56 +0900

    [Silver I] Title: 곱셈, Time: 100 ms, Memory: 14084 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -2,31 +2,32 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        long C = Long.parseLong(st.nextToken());
 
-		long A = Long.parseLong(st.nextToken());
-		long B = Long.parseLong(st.nextToken());
-		long C = Long.parseLong(st.nextToken());
+        long ret = find(A, B, C);
 
+        bw.write(String.valueOf(ret));
+        bw.flush();
+    }
 
+    static long find(long a, long b, long c) {
+        if (b == 1) {
+            return a % c;
+        }
+        long ret = find(a, b / 2, c);
+        ret = ret * ret % c;
 
-		bw.write(String.valueOf(recursion(A, B, C)));
-		bw.flush();
-	}
-
-	static long recursion(long A, long B, long C) {
-		if (B == 1)
-			return A % C;
-		long result = recursion(A, B / 2, C);
-		result = result * result % C;
-		
-		if (B % 2 == 0) // 짝수라면 바로 리턴
-			return result;
-		// 홀수라면 한번 더 계산한 후 리턴
-		return result * A % C;
-	}
+        if (b % 2 == 1) {
+            ret = ret * a % c;
+        }
+        
+        return ret;
+    }
 }
