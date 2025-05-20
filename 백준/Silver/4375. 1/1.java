c6e72c6fc3ff814d47435c3b4d1f391b29feb117
박지혁 <pjh5365@naver.com>
2025-05-20 21:01:05 +0900

    [Silver III] Title: 1, Time: 100 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.java
+++ 백준/Silver/4375. 1/1.java
@@ -1,26 +1,27 @@
 import java.io.*;
+import java.math.BigDecimal;
 import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
+        String input;
+        while ((input = br.readLine()) != null) {
+            int n = Integer.parseInt(input);
+            int num = 0;
+            for (int i = 1; ; i++) {
+                num = (num * 10 + 1) % n;
+                if (num == 0) {
+                    sb.append(i).append("\n");
+                    break;
+                }
+            }
+        }
 
-		String s;
-		while ((s = br.readLine()) != null) {
-			int N = Integer.parseInt(s);
-			int mod = 0;
-			for (int i = 1; ; i++) {
-				mod = (mod * 10 + 1) % N;
-				if (mod == 0) {
-					sb.append(i).append("\n");
-					break;
-				}
-			}
-		}
-		bw.write(String.valueOf(sb));
-		bw.flush();
-	}
+        bw.write(String.valueOf(sb));
+        bw.flush();
+    }
 }
