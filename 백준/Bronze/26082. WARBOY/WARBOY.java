0b598f165828b13a9996e3e001fe869725b0d6bf
박지혁 <pjh5365@naver.com>
2025-05-29 07:25:42 +0900

    [Bronze V] Title: WARBOY, Time: 100 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        int ret = B / A * C * 3;
+
+        bw.write(String.valueOf(ret));
+        bw.flush();
+    }
+}
