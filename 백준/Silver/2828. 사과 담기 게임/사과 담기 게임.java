10203d74dd6dc6d40e06cadc80e603c8fff51b35
박지혁 <pjh5365@naver.com>
2025-05-28 21:34:15 +0900

    [Silver V] Title: 사과 담기 게임, Time: 108 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.java
+++ 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.java
@@ -2,32 +2,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int T = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int sIndex = 1;
-		int eIndex = M;
-		int result = 0;
-		for (int i = 0; i < T; i++) {
-			int apple = Integer.parseInt(br.readLine());
-			if (apple < sIndex) {
-				result += sIndex - apple;
-				eIndex -= sIndex - apple;
-				sIndex = apple;
-			} else if (apple > eIndex) {
-				result += apple - eIndex;
-				sIndex += apple - eIndex;
-				eIndex = apple;
-			}
-		}
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-		bw.write(String.valueOf(result));
-		bw.flush();
-	}
+        int ret = 0;
+        int start = 1;
+        int end = M;
+        int J = Integer.parseInt(br.readLine());
+        while (J-- > 0) {
+            int now = Integer.parseInt(br.readLine());
+            if (now < start) {
+                int move = start - now;
+                end -= move;
+                start -= move;
+                ret += move;
+            } else if (now > end) {
+                int move = now - end;
+                end += move;
+                start += move;
+                ret += move;
+            }
+        }
+
+        bw.write(String.valueOf(ret));
+        bw.flush();
+    }
 }
