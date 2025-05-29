04457a2e477163458baa65786ceaa9075fca87ba
박지혁 <pjh5365@naver.com>
2025-05-29 20:54:35 +0900

    [Silver III] Title: 빈도 정렬, Time: 140 ms, Memory: 16320 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
@@ -2,41 +2,33 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int C = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
 
-		st = new StringTokenizer(br.readLine());
-		Map<Integer, Integer> map = new LinkedHashMap<>(); // 키값 순서보장을 위한 LinkedHashMap
-		for (int i = 0; i < N; i++) {
-			int now = Integer.parseInt(st.nextToken());
-			if (map.containsKey(now)) {
-				map.put(now, map.get(now) + 1);
-			} else {
-				map.put(now, 1);
-			}
-		}
+        st = new StringTokenizer(br.readLine());
+        ArrayList<Integer> list = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            int now = Integer.parseInt(st.nextToken());
+            map.put(now, map.getOrDefault(now, 0) + 1);
+            if (map.get(now) == 1) {
+                list.add(now);
+            }
+        }
 
-		List<Integer> list = new ArrayList<>(map.keySet());
-		list.sort(new Comparator<Integer>() {
-			@Override
-			public int compare(Integer o1, Integer o2) {
-				return map.get(o2).compareTo(map.get(o1));
-			}
-		});
-		list.forEach(key -> {
-			for (int i = 0; i < map.get(key); i++) {
-				sb.append(key).append(" ");
-			}
-		});
+        Collections.sort(list, (o1, o2) -> map.get(o2) - map.get(o1));
 
-		bw.write(String.valueOf(sb));
-		bw.flush();
-	}
+        for (int i : list) {
+            for (int j = 0; j < map.get(i); j++) {
+                bw.write(i + " ");
+            }
+        }
+        bw.flush();
+    }
 }
