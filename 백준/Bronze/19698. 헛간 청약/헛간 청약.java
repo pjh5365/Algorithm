64c31439d55f02de654927ee37dd2676c99868b1
박지혁 <pjh5365@naver.com>
2025-06-13 07:22:54 +0900

    [Bronze IV] Title: 헛간 청약, Time: 120 ms, Memory: 14596 KB -BaekjoonHub

---
+++ 백준/Bronze/19698. 헛간 청약/헛간 청약.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int W = Integer.parseInt(st.nextToken());
+        int H = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        int ret = (W / L) * (H / L);
+
+        bw.write(Math.min(N, ret) + " ");
+        bw.flush();
+    }
+}
