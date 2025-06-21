ecc96e6c818eaf8e3e466a88fbe2215d2db12016
박지혁 <pjh5365@naver.com>
2025-06-21 16:30:54 +0900

    [Bronze IV] Title: 등교, Time: 116 ms, Memory: 14324 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/31962. 등교/등교.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+
+        int ret = -1;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int S = Integer.parseInt(st.nextToken());
+            int T = Integer.parseInt(st.nextToken());
+
+            if (S + T > X) {
+                continue;
+            }
+
+            ret = Math.max(ret, S);
+        }
+
+        bw.write(ret + "");
+        bw.flush();
+    }
+}
