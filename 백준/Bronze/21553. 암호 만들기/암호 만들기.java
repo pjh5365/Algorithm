4b88e9d8fe85ab1ad1871cc3cc2a7d42aadb506d
박지혁 <pjh5365@naver.com>
2025-07-03 12:58:25 +0900

    [Bronze III] Title: 암호 만들기, Time: 104 ms, Memory: 14328 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21553. 암호 만들기/암호 만들기.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String A = br.readLine();
+        String P = br.readLine();
+
+        bw.write(P + "");
+        bw.flush();
+    }
+}
