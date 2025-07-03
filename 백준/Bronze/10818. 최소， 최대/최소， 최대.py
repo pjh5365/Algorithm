18ab7d8d46adf80d402ebfc75ea80259b7281979
박지혁 <pjh5365@naver.com>
2025-07-03 16:58:23 +0900

    [Bronze III] Title: 최소, 최대, Time: 316 ms, Memory: 129196 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.py
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -1,12 +1,3 @@
 N = int(input())
-arr = list(map(int, input().split()))
-max_num = arr[0]
-min_num = arr[0]
-
-for num in arr:
-	if num > max_num:
-		max_num = num
-	elif num < min_num:
-		min_num = num
-
-print(min_num, max_num)
+arr=list(map(int, input().split()))
+print(min(arr), max(arr))
