aba82a082a583348bcb0098d45e523a115967410
박지혁 <pjh5365@naver.com>
2025-11-12 19:46:06 +0900

    [Bronze IV] Title: DORO, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/34073. DORO/DORO.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            sb.append(st.nextToken()).append("DORO").append(" ");
+        }
+
+        bw.write(sb + "\n");
+        bw.flush();
+    }
+}
