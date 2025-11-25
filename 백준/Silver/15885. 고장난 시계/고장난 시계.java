34fb1d0de45e10a44f8d38ccfa3ec2ea37770e57
박지혁 <pjh5365@naver.com>
2025-11-25 20:13:16 +0900

    [Silver V] Title: 고장난 시계, Time: 104 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Silver/15885. 고장난 시계/고장난 시계.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int ret = (int)(Math.abs((double)a / b - 1) * 2);
+
+        bw.write(String.valueOf(ret));
+        bw.flush();
+    }
+}
