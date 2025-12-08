9d8c2eb4080a147bfa1576fd977c4a593514ca91
박지혁 <pjh5365@naver.com>
2025-12-08 17:16:47 +0900

    [Bronze II] Title: 서강의 역사를 찾아서, Time: 108 ms, Memory: 14400 KB -BaekjoonHub

---
+++ 백준/Bronze/25177. 서강의 역사를 찾아서/서강의 역사를 찾아서.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int max = Math.max(N, M);
+        int[] arr1 = new int[max];
+        int[] arr2 = new int[max];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr1[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            arr2[i] = Integer.parseInt(st.nextToken());
+        }
+        int ret = 0;
+        for (int i = 0; i < max; i++) {
+            int tmp = arr2[i] - arr1[i];
+            ret = Math.max(ret, tmp);
+        }
+
+        bw.write(ret + "");
+        bw.flush();
+    }
+}
