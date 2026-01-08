0a6c91a080d806c80a5aba0c60ba82b74dc3a3e4
박지혁 <pjh5365@naver.com>
2026-01-08 22:43:29 +0900

    [Bronze III] Title: 삼각 무늬 - 1, Time: 108 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/11320. 삼각 무늬 － 1/삼각 무늬 － 1.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            sb.append((int)Math.pow(A / B, 2)).append("\n");
+        }
+
+        bw.write(sb + "");
+        bw.flush();
+    }
+}
