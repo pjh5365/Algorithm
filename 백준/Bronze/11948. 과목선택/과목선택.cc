c704623cbac422d77d08b232f1287268f6e4a0e7
박지혁 <pjh5365@naver.com>
2026-01-26 19:25:53 +0900

    [Bronze IV] Title: 과목선택, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11948. 과목선택/과목선택.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int a[4];
+  int b[2];
+
+  for (int i = 0; i < 4; i++) {
+    cin >> a[i];
+  }
+  for (int i = 0; i < 2; i++) {
+    cin >> b[i];
+  }
+
+  sort(a, a + 4);
+  sort(b, b + 2);
+
+  cout << (a[1] + a[2] + a[3] + b[1]);
+}
