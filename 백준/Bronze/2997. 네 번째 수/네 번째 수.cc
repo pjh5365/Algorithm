8888a0c08932ae52e258ffbfed8aebba614c7497
박지혁 <pjh5365@naver.com>
2026-02-06 19:58:19 +0900

    [Bronze III] Title: 네 번째 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2997. 네 번째 수/네 번째 수.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int arr[3];
+  cin >> arr[0] >> arr[1] >> arr[2];
+  sort(arr, arr + 3);
+  int a = arr[1] - arr[0];
+  int b = arr[2] - arr[1];
+
+  if (a > b) cout << arr[0] + b;
+  else if (a == b) cout << arr[0] - a;
+  else cout << arr[1] + a;
+
+  return 0;
+}
