8c8530c98642a55322d038f6778e2e43b1828e1c
박지혁 <pjh5365@naver.com>
2026-02-11 19:42:29 +0900

    [Bronze V] Title: 찾아오시는 길, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34217. 찾아오시는 길/찾아오시는 길.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+  if (a + c < b + d) {
+    cout << "Hanyang Univ.";
+  } else if (a + c > b + d) {
+    cout << "Yongdap";
+  } else {
+    cout << "Either";
+  }
+
+  return 0;
+}
