43789f0de37e0cd3a4a57247b7d3032f45d23c91
박지혁 <pjh5365@naver.com>
2026-02-21 14:34:12 +0900

    [Bronze III] Title: 아카라카 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32652. 아카라카 2/아카라카 2.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int k;
+    cin >> k;
+    cout << "AKARAKA";
+    for (int i = 1; i < k; i++) cout << "RAKA";
+
+    return 0;
+}
