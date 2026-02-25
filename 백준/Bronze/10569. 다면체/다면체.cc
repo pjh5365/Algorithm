e6d4b702b295a9f969817ac957a8f669e99d5c7c
박지혁 <pjh5365@naver.com>
2026-02-25 19:46:38 +0900

    [Bronze IV] Title: 다면체, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10569. 다면체/다면체.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t, v, e;
+    cin >> t;
+    while (t--) {
+      cin >> v >> e;
+      cout << 2 - v + e << "\n";
+    }
+
+    return 0;
+}
