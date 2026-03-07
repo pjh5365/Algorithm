fb19755db70d357670f320edb81cb24f8909a570
박지혁 <pjh5365@naver.com>
2026-03-07 13:58:22 +0900

    [Bronze IV] Title: 주사위, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int t, a, b;
+  cin >> t;
+  for (int i = 1; i <= t; i++) {
+    cin >> a >> b;
+    printf("Case %d: %d \n", i, a + b);
+  }
+}
