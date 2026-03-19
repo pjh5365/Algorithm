8e09ad886b0176e62fb93a89e12c793721aa2d7d
박지혁 <pjh5365@naver.com>
2026-03-19 19:42:28 +0900

    [Bronze II] Title: 방 배정하기, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/14697. 방 배정하기/방 배정하기.cc
+++ 백준/Bronze/14697. 방 배정하기/방 배정하기.cc
@@ -5,9 +5,9 @@ int main() {
   int a, b, c, n;
   cin >> a >> b >> c >> n;
 
-  for (int i = 1; i <= n / a; i++) {
-    for (int j = 1; j <= n / b; j++) {
-      for (int k = 1; k <= n / c; k++) {
+  for (int i = 0; i <= n / a; i++) {
+    for (int j = 0; j <= n / b; j++) {
+      for (int k = 0; k <= n / c; k++) {
         int tmp = n;
         tmp -= a * i;
         tmp -= b * j;
