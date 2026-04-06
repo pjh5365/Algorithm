54f18e1fb356d713030ab6aa92c9b1f97ff3cd22
박지혁 <pjh5365@naver.com>
2026-04-06 20:43:32 +0900

    [Bronze IV] Title: 달달함이 넘쳐흘러, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17256. 달달함이 넘쳐흘러/달달함이 넘쳐흘러.cc
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a, b, c, d, e, f;
+  cin >> a >> b >> c;
+  cin >> d >> e >> f;
+  cout << d - c << " " << e / b << " "  << f - a;
+}
