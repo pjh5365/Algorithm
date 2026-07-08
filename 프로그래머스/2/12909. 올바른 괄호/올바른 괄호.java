ab73bd145210b72529b01b679f43a655040662f0
박지혁 <pjh5365@naver.com>
2026-07-08 20:39:17 +0900

    [level 2] Title: 올바른 괄호, Time: 15.06 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -4,14 +4,16 @@ class Solution {
     boolean solution(String s) {
         Stack<Character> stack = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(') {
-                stack.push('(');
+            char now = s.charAt(i);
+            if (stack.isEmpty()) {
+                stack.push(now);
+                continue;
+            }
+            if (now == ')' && stack.peek() == '(') {
+                stack.pop();
+                continue;
             } else {
-                if (stack.size() > 0) {
-                    stack.pop();
-                } else {
-                    return false;
-                }
+                stack.push(now);
             }
         }
 
