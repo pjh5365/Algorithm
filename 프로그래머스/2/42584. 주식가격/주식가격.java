90de06e563479ba49212b39da1652f64777ae764
박지혁 <pjh5365@naver.com>
2026-07-08 20:39:33 +0900

    [level 2] Title: 주식가격, Time: 22.70 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,28 +1,25 @@
 import java.util.*;
 
 class Solution {
-    /**
-    * prices의 idx를 스택에 넣어 하나의 스택만 사용하기
-    */
     public int[] solution(int[] prices) {
-        Stack<Integer> s = new Stack<>(); // 가격을 idx를 담을 스택
         int len = prices.length;
-        int[] answer = new int[len];
-        
-        s.push(0); // 0번 idx는 무조건 입력
-        
+        int[] ret = new int[len];
+
+        Stack<Integer> s = new Stack<>();
+        s.push(0);
         for (int i = 1; i < len; i++) {
-            // 스택이 비어있지 않고, 새로운 가격이 현재 가격보다 낮다면 (가격이 떨어졌다면)
             while (!s.isEmpty() && prices[i] < prices[s.peek()]) {
-                int tmp = s.pop();
-                answer[tmp] = i - tmp; // 기존의 가격에 떨어지지 않은 길이 입력하기
+                int idx = s.pop();
+                ret[idx] = i - idx;
             }
-            s.push(i); // 가격이 떨어지지 않았다면
+            s.push(i);
         }
-        while (!s.isEmpty()) { // 스택에 남은 나머지 연산하기
-            int tmp = s.pop();
-            answer[tmp] = len - 1 - tmp;
+        
+        while (!s.isEmpty()) {
+            int idx = s.pop();
+            ret[idx] = len - 1 - idx;
         }
-        return answer;
+        
+        return ret;
     }
 }
