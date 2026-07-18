62606a7c6425738d924598e38b46cc326412e235
박지혁 <pjh5365@naver.com>
2026-07-18 21:55:51 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 30.87 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -1,38 +1,41 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        HashMap<String, String> p = new HashMap<>();
-        HashMap<String, Integer> m = new HashMap<>();
-        
+        HashMap<String, Integer> m1 = new HashMap<>(); // 이름 -> 이익
+        HashMap<String, String> m2 = new HashMap<>(); // 자식 -> 부모
+
+        m1.put("center", 0);
+
         for (int i = 0; i < enroll.length; i++) {
-            p.put(enroll[i], referral[i]);
-            m.put(enroll[i], i);
-        }
-        
-        int[] answer = new int[enroll.length];
-        
-        for (int i = 0; i < seller.length; i++) {
-            String s = seller[i];
-            
-            int money = amount[i] * 100;
-            
-            while (!s.equals("-")) {
-                int parent = money / 10;
-                int my = money - parent;
-                
-                answer[m.get(s)] += my;
-                
-                s = p.get(s);
-                money /= 10;
-                
-                if (money < 1) {
-                    break;
-                }
+            String name = enroll[i];
+            String parent = referral[i];
+            if (parent.equals("-")) {
+                m1.put(name, 0);
+                m2.put(name, "center");
+            } else {
+                m1.put(name, 0);
+                m2.put(name, parent);
             }
         }
-        
-        return answer;
+
+        for (int i = 0; i < seller.length; i++) {
+            String name = seller[i];
+            int money = amount[i] * 100;
+
+            while (money > 0 && !name.equals("center")) {
+                m1.put(name, m1.getOrDefault(name, 0) + money - (money / 10));
+                name = m2.get(name);
+                money /= 10;
+            }
+        }
+
+        int[] ret = new int[enroll.length];
+
+        for (int i = 0; i < ret.length; i++) {
+            ret[i] = m1.get(enroll[i]);
+        }
+
+        return ret;
     }
 }
