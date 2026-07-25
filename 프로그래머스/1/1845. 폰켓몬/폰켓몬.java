09b438e9b8675db4a2a0c8cf16b322f2f60be7f5
박지혁 <pjh5365@naver.com>
2026-07-25 18:35:23 +0900

    [level 1] Title: 폰켓몬, Time: 6.89 ms, Memory: 90.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        Set<Integer> s = new HashSet<>();
+        
+        for (int i : nums) {
+            s.add(i);
+        }
+        
+        if (s.size() > nums.length / 2) {
+            return nums.length / 2;
+        }
+        return s.size();    
+    }
+}
